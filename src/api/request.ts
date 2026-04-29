7819d0183aec57bcbb0ec40ad87121a26cb55a85
ZhangX1n <390755481@qq.com>
2026-04-29 16:00:45 +0800

    api拦截器修改

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import router from "@/router";
 import { getToken, removeToken } from "@/utils/token";
-
+import { message } from "antd";
 const request = axios.create({
   // 从环境变量中读取 API 基础路径
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000
 });
 
@@ -25,14 +25,14 @@ request.interceptors.request.use(
 request.interceptors.response.use(
   result => {
     return result.data
-
   },
   error => {
-    // if (error.response?.status === 401) {
-    //   removeToken()
-    //   router.navigate('/login')
-    //   window.location.reload()
-    // }
+    if (error.response?.status === 401) {
+      removeToken()
+      router.navigate('/login')
+      message.destroy()
+      message.error('登录已过期，请重新登录')
+    }
     return Promise.reject(error)
   }
 )
