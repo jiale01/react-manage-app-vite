4c7f446f62aeabf89603a0dcd5ac5ce6d215f4dc
ZhangX1n <390755481@qq.com>
2026-04-29 16:00:24 +0800

    异步组件优化

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,16 @@
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
-import Dashboard from "@/pages/Dashboard";
-import ArticleCreate from "@/pages/Article/Create";
-import ArticleList from "@/pages/Article/List";
+// import Dashboard from "@/pages/Dashboard";
+// import ArticleCreate from "@/pages/Article/Create";
+// import ArticleList from "@/pages/Article/List";
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
 import { AuthRoute } from "@/components/AuthRoute";
+import { lazy, Suspense } from "react";
+
+const Dashboard = lazy(() => import("@/pages/Dashboard"));
+const ArticleCreate = lazy(() => import("@/pages/Article/Create"));
+const ArticleList = lazy(() => import("@/pages/Article/List"));
 
 // 定义路由配置类型
 const routes: RouteObject[] = [
@@ -17,19 +22,19 @@ const routes: RouteObject[] = [
     children: [
       {
         index: true,
-        element: <Dashboard />,
+        element: <Suspense fallback={<div>Loading...</div>}><Dashboard /></Suspense>,
       },
       {
         path: "dashboard",
-        element: <Dashboard />,
+        element: <Suspense fallback={<div>Loading...</div>}><Dashboard /></Suspense>,
       },
       {
         path: "/article/list",
-        element: <ArticleList />,
+        element: <Suspense fallback={<div>Loading...</div>}><ArticleList /></Suspense>,
       },
       {
         path: "/article/create",
-        element: <ArticleCreate />,
+        element: <Suspense fallback={<div>Loading...</div>}><ArticleCreate /></Suspense>,
       },
     ],
   },
